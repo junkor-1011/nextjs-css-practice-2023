53e78440c3bd72c935a8260a06266a4ef7d308ea
junkor-1011 <tomoyukizxy@gmail.com>
2023-07-05 00:08:28 +0900

    format

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-import Image from 'next/image'
+import React from 'react';
+import Image from 'next/image';
 
-export default function Home() {
+const Home: React.FC = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -109,5 +110,6 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
-}
+  );
+};
+export default Home;
